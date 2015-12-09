75d8d8bba07bcae0d78f348808ff4cee8acb9f5e
objektwerks <objektwerks@gmail.com>
2015-12-09 13:22:29 -0500

    added xs and ys data to app

--- src/main/scala/stats/StatsApp.scala
+++ src/main/scala/stats/StatsApp.scala
@@ -6,13 +6,24 @@ import scalafx.scene.Scene
 import scalafx.scene.control._
 import scalafx.scene.layout.VBox
 
-class StatsApp extends JFXApp {
+object StatsApp extends JFXApp {
+  private val xs = Vector[Double](1.0, 2.0, 3.0, 3.0, 4.0, 4.0, 5.0)
+  private val ys = Vector[Double](1.5, 2.5, 3.25, 3.5, 4.25, 4.75, 5.5)
+
+  val xsLabel = new Label {
+    text = "XS Data: " + xs.mkString(", ")
+  }
+
+  val ysLabel = new Label {
+    text = "YS Data: " + ys.mkString(", ")
+  }
+
   val content = new VBox {
     maxWidth = 400
     maxHeight = 400
     spacing = 6
     padding = Insets(6)
-    children = List()
+    children = List(xsLabel, ysLabel)
   }
 
   val toolbar = new ToolBar {
