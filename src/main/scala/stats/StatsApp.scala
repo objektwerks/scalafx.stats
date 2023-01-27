f9418285754d5a1705279683b5d4f438129c4337
objektwerks <objektwerks@runbox.com>
2023-01-27 02:50:01 -0500

    added logging to app

--- src/main/scala/stats/StatsApp.scala
+++ src/main/scala/stats/StatsApp.scala
@@ -1,13 +1,16 @@
 package stats
 
+import com.typesafe.scalalogging.LazyLogging
+
 import scalafx.application.JFXApp3
 
-object StatsApp extends JFXApp3:
+object StatsApp extends JFXApp3 with LazyLogging:
   override def start(): Unit =
     val view = StatsView()
     stage = new JFXApp3.PrimaryStage {
-    scene = view.scene
-    title = "Stats"
-  }
+      scene = view.scene
+      title = "Stats"
+    }
+    logger.info("*** Stats app started.")
 
-  override def stopApp(): Unit = ???
+  override def stopApp(): Unit = logger.info("*** Stats app stopped.")
